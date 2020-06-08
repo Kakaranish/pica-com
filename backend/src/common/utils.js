1534e57940e314b59ad549a32be5eb507142d78f
Kakaranish <stasiekgruz@gmail.com>
2020-06-08 21:39:12 +0200

    moved request handler from backend to frontend

--- backend/src/common/utils.js
+++ backend/src/common/utils.js
@@ -37,30 +37,3 @@ export const withAsyncRequestHandler = async (res, action) => {
         res.status(500).json({ errors: ['Internal error'] });
     }
 };
-
-/**
- * @async
- * @param {function: any} action 
- * @param  {Array.<{status: string, function: void}>} handlers 
- * @returns {Promise.<any>} result
- * @example 
- * await requestHandler(
- *  async () => axios.post('/some/uri'/),
- *  {status: 404, callback: async () => console.log('custom 404 error handling')},
- *  {status: 500, callback: async () => {window.location = '/error/500'}},
- * );
- */
-export const requestHandler = async (action, ...handlers) => {
-    const result = await action();
-    if (result.status === 200) return result.data;
-
-    handlers = handlers ?? [];
-    let handlersDict = Object.assign({}, ...handlers.map(h => ({ [h.status]: h.callback })));
-    
-    const handler = handlersDict[result.status];
-    if (handler) return await handler.callback();
-    
-    window.location = `/error/${result.status}`;
-}
-
-requestHandler()
